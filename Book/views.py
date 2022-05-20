faecf7add480ccadf6d3ce22226351ecc4e23052
yongCloude <djkye0001@naver.com>
2022-05-20 16:24:18 +0900

    [feat] GET 요청을 처리해주는 함수 추가

--- Book/views.py
+++ Book/views.py
@@ -1,6 +1,11 @@
+from xmlrpc.client import ResponseError
 from rest_framework import viewsets, permissions, generics, status
 from rest_framework import Response
 from rest_framework import APIView
 from rest_framework import api_view
 
 # Create your views here.
+
+@api_view(['GET'])
+def HelloAPI(request) :
+    return Response("hello world!")
