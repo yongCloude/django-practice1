77d96c6928b8b0ddb2f1b5f64a3c29a5d9404008
yongCloude <djkye0001@naver.com>
2022-05-20 16:22:46 +0900

    [doc] DEF에서 사용하는 module 추가

--- Book/views.py
+++ Book/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework import Response
+from rest_framework import APIView
+from rest_framework import api_view
 
 # Create your views here.
