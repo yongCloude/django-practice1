01ecd02a51fad83dd97da8a6667981a12ba66593
yongCloude <djkye0001@naver.com>
2022-05-21 17:09:10 +0900

    [update] BookAPI generic View url path 추가

--- Book/urls.py
+++ Book/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import BookAPIMixins, BooksAPIMixins, HelloAPI, booksAPI, bookAPI, BookAPI, BooksAPI
+from .views import BookAPIGenerics, BookAPIMixins, BooksAPIGenerics, BooksAPIMixins, HelloAPI, booksAPI, bookAPI, BookAPI, BooksAPI
 
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path("cbv/books/", BooksAPI.as_view()),
     path("cbv/book/<int:bid>/", BookAPI.as_view()),
     path("mixin/books/", BooksAPIMixins.as_view()),
-    path("mixin/book/<int:bid>/", BookAPIMixins.as_view())
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view()),
+    path("generic/books/", BooksAPIGenerics.as_view()),
+    path("generic/book/<int:bid>/", BookAPIGenerics.as_view()),
 ]
