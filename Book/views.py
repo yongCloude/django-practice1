c747a5fb3f98c92284074654a84a58d442f4354e
yongCloude <djkye0001@naver.com>
2022-05-21 16:54:01 +0900

    [feat] BookAPI View - mixins put, delete 추가

--- Book/views.py
+++ Book/views.py
@@ -70,6 +70,10 @@ class BookAPIMixins(mixins.RetrieveModelMixin, generics.GenericAPIView) :
 
     def get(self, request, *args, **kwargs) :
         return self.retrieve(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs) :
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs) : 
+        return self.destroy(request, *args, **kwargs)
         
     
     
