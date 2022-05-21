67db02a92ba5f09ed587ad5816925e4eb719aace
yongCloude <djkye0001@naver.com>
2022-05-21 16:55:22 +0900

    [update] BookAPI View mixins url path 추가

--- Book/urls.py
+++ Book/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import HelloAPI, booksAPI, bookAPI, BookAPI, BooksAPI
+from .views import BookAPIMixins, BooksAPIMixins, HelloAPI, booksAPI, bookAPI, BookAPI, BooksAPI
 
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path("fbv/book/<int:bid>/", bookAPI),
     path("cbv/books/", BooksAPI.as_view()),
     path("cbv/book/<int:bid>/", BookAPI.as_view()),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view())
 ]
