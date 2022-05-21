12b87855a4f07bc4c9752491c2384c4d9887d305
yongCloude <djkye0001@naver.com>
2022-05-21 15:12:41 +0900

    [feat] Book API cbv

--- Book/views.py
+++ Book/views.py
@@ -28,7 +28,28 @@ def bookAPI(request, bid) :
     book = get_object_or_404(Book, bid = bid)
     serializer = BookSerializer(book)
     return Response(serializer.data, status.status.HTTP_200_OK)
+
+
+class BooksAPI(APIView) :
+    def get(self, request) :
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
     
+    def post(self, request) : 
+        serializer = BookSerializer(data = request.data)
+        if serializer.is_valid() :
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+class BookAPI(APIView) :
+    def get(self, request, bid) :
+        book = get_object_or_404(Book, bid = bid)
+        serializer = BookSerializer(book)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
 
 
     
